990bf62250f606d7342518f35ac3fdcb98af0475
Wonder Diaz <wonderdxk@gmail.com>
2019-04-09 00:56:24 -0400

    1.0.1
    - Se colocaron iconos para acciones
    - Se agregaron las opciones para agregar y borrar notas (En
    Construccion)
    - Se agregaron botones de opciones en el Sidebar (Maquetado)
    - Estilos basico de la aplicacion
    - Se cambiaron las notas de ejemplos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+ /*Materialize CSS*/
  document.addEventListener('DOMContentLoaded', function () {
      var elems = document.querySelectorAll('.sidenav');
      var instances = M.Sidenav.init(elems);
@@ -14,17 +15,63 @@
      });
  });
 
+ /*Angular*/
  var AppNotes = angular.module('AppNote', ['ngRoute']);
-
+ /*Titulo*/
  AppNotes.controller('Titulo', ["$scope", function ($scope) {
      $scope.title = "Notes";
  }]);
- /*Filtros*/
- AppNotes.controller('filtroNotas', ['$scope', '$http', function ($scope, $http) {
-     $http({method: 'GET',url: 'js/notes.json'
+ /*Usuario y Correo*/
+ AppNotes.controller('Usuario', ["$scope", function ($scope) {
+     $scope.user = "MetalSyntax";
+     $scope.email = "metalsyntax@gmail.com";
+ }]);
+ /*Obtencion de Datos
+ AppNotes.controller('NotasDemo', ['$scope', '$http', function ($scope, $http) {
+     $http({
+         method: 'GET',
+         url: 'js/notes.json'
      }).then(function (Notes) {
-        $scope.ItemNotes = Notes.data;
+         $scope.ItemNotes = Notes.data;
      }).then(function (error) {
          //console.log(error);
      });
+ }]);*/
+ /*Crear Notas*/
+ AppNotes.controller('NewNotes', ['$scope', function ($scope) {
+    //Notas de Prueba
+    $scope.NoteList = [{
+             title: 'Do nothing',
+             message: 'Lorem'
+         },
+         {
+             title: 'Show some tasks',
+             message: 'Lorem'
+         },
+         {
+             title: 'Add a task',
+             message: 'Lorem'
+         },
+         {
+             title: 'Walk the dog',
+             message: 'Lorem'
+         }, {
+             title: 'New a task',
+             message: 'Lorem'
+         }, {
+             title: 'Eat Hotdog',
+             message: 'Lorem'
+         }
+     ];
+     //Agregar una Nueva Nota
+     $scope.add = function (title,message) {
+       $scope.NoteList.push({
+        title: title,
+        message: message
+       });
+     };
+     //Borrar una Nota
+     $scope.delete = function () {
+        $scope.NoteList.splice(this.$index, 1);
+     };
  }]);
